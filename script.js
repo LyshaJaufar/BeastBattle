3d3f9b307f93a2c15035539a056ce5d5dfa5f389
LyshaJaufar <lysha.ahmd@gmail.com>
2022-02-15 20:18:51 +0500

    upd

--- script.js
+++ script.js
@@ -13,51 +13,70 @@ const playerCardSlot3 = document.querySelector(".player-card-slot3")
 
 var wholeDeck;
 var playerDeck, computerDeck;
-var playerCard0, playerCard1, playerCard2, playerCard3;
-var roundStarted = false;
+var playerCard0, playerCard1, playerCard2, playerCard3, computerCard0, computerCard1, computerCard2, computerCard3;
+var roundStarted;
+var playerBoxes;
 
 var i = 0
 
 startGame()
 function startGame() {
+    setupGame()
+}
+
+function setupGame() {
     wholeDeck = new Deck()
     //deck.shuffle()
     const deckMidpoint = Math.ceil(wholeDeck.numberOfCards / 2)
     playerDeck = new Deck(wholeDeck.cards.slice(0, deckMidpoint))
     computerDeck = new Deck(wholeDeck.cards.slice(deckMidpoint, wholeDeck.numberOfCards))
 
-    playerCard0 = createCard(playerDeck, playerCardSlot0, "flip-player-card0")
-    playerCard1 = createCard(playerDeck, playerCardSlot1, "flip-player-card1")
-    playerCard2 = createCard(playerDeck, playerCardSlot2, "flip-player-card2")
-    playerCard3 = createCard(playerDeck, playerCardSlot3, "flip-player-card3")
+    roundStarted = false
+
+    playerCard0 = createCard(playerDeck, playerCardSlot0)
+    playerCard1 = createCard(playerDeck, playerCardSlot1)
+    playerCard2 = createCard(playerDeck, playerCardSlot2)
+    playerCard3 = createCard(playerDeck, playerCardSlot3)
 
     playerCard0.classList.add('playerCard')
     playerCard1.classList.add('playerCard')
     playerCard2.classList.add('playerCard')
     playerCard3.classList.add('playerCard')
 
-    createCard(computerDeck, computerCardSlot0)
-    createCard(computerDeck, computerCardSlot1)
-    createCard(computerDeck, computerCardSlot2)
-    createCard(computerDeck, computerCardSlot3)
+    computerCard0 = createCard(computerDeck, computerCardSlot0)
+    computerCard1 = createCard(computerDeck, computerCardSlot1)
+    computerCard2 = createCard(computerDeck, computerCardSlot2)
+    computerCard3 = createCard(computerDeck, computerCardSlot3)
 
-
+    computerCard0.classList.add('computerCard')
+    computerCard1.classList.add('computerCard')
+    computerCard2.classList.add('computerCard')
+    computerCard3.classList.add('computerCard')
 }
 
 document.getElementById('button').onclick = function() {
-    playerDeckFlip()
-    roundStarted = true;
+    deckFlip()
+};
+
+document.getElementById('battleButton').onclick = function() {
+    allHaveAttribute()
+    function allHaveAttribute() {
+        for (var i = 0; i < playerBoxes.length; i++) {
+            if (playerBoxes[i].getAttribute("empty") !== "false") {
+            }
+        }
+        battlePhase()
+    }
 };
 
 var cards = Array.from(document.getElementsByClassName("card"))
 cards.forEach(card => {
     card.addEventListener("dragstart", () => {
-        console.log("checking")
         card.classList.add("zoomCard")
     });
 });
 
-function createCard(deck, cardSlot, flip) {
+function createCard(deck, cardSlot) {
     var cardImg = document.createElement('IMG')
     var cardText = document.createElement('h1')
     //var cardDiv = document.createElement('div')
@@ -92,62 +111,82 @@ function createCard(deck, cardSlot, flip) {
     //return cardDiv
 }
 
-const boxes = document.querySelectorAll('.box');
-var draggables = document.querySelectorAll('.maincontainer')
-
-draggables.forEach(draggable => {
-    draggable.addEventListener('dragstart', () => {
-        draggable.classList.add("dragging")
-            setTimeout(() => {
-                draggable.classList.add('hide');
-        }, 0);
-    })
-    draggable.addEventListener('dragend', () => {
-        draggable.classList.remove('dragging')
-        draggable.classList.remove('hide');
-    })
-
-})
-
-boxes.forEach(box => {
-    box.addEventListener('dragover', e => {
-        e.preventDefault()
-        e.target.classList.remove('drag-over');
-        const afterElement = getDragAfterElement(box, e.clientY)
-        const draggable = document.querySelector('.dragging')
-        if (afterElement == null) {
-            box.appendChild(draggable)
-            createCard()
-        }
-
-    })
-})
-
-function getDragAfterElement(box, y) {
-    var draggableElements = [...box.querySelectorAll('.maincontainer:not(.dragging)')]
-    
-    return draggableElements.reduce((closest, child) => {
-        const boxBound = child.getBoundingClientRect()
-        const offset = y - boxBound.top - boxBound.height / 2
-        if (offset < 0 && offset > closest.offset) {
-            return { offset: offset, element: child } 
-        } else {
-            return closest
-        }
-    }, { offset: Number.NEGATIVE_INFINITY }).element
-}
 
 
-function setPlayerDeck() {
-    console.log('test')
-}
-
-
-function playerDeckFlip() {
+function deckFlip() {
     if (!roundStarted){
         playerCard0.classList.toggle("flip-player-card0")
         playerCard1.classList.toggle("flip-player-card1")
         playerCard2.classList.toggle("flip-player-card2")
         playerCard3.classList.toggle("flip-player-card3")
+        computerCard0.classList.toggle("flip-computer-card0")
+        computerCard1.classList.toggle("flip-computer-card1")
+        computerCard2.classList.toggle("flip-computer-card2")
+        computerCard3.classList.toggle("flip-computer-card3")
+
+        roundStarted = true
+
+        startPhase()
     }
+}
+
+function startPhase(){
+    playerBoxes = document.querySelectorAll('.playerBox');
+    var playerBoxes1 = Array.from(document.querySelectorAll('.playerBox'))
+    playerBoxes1.forEach(playerBox1 => {
+        playerBox1.setAttribute("empty", "true")
+    })
+    var draggables = document.querySelectorAll('.maincontainer')
+
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', () => {
+            draggable.classList.add("dragging")
+                setTimeout(() => {
+                    draggable.classList.add('hide');
+            }, 0);
+        })
+        draggable.addEventListener('dragend', () => {
+            draggable.classList.remove('dragging')
+            draggable.classList.remove('hide');
+        })
+
+    })
+
+
+    playerBoxes.forEach(playerBox => {
+        playerBox.addEventListener('dragover', e => {
+            e.preventDefault()
+            e.target.classList.remove('drag-over');
+            const afterElement = getDragAfterElement(playerBox, e.clientY)
+            const draggable = document.querySelector('.dragging')
+            if (afterElement == null) {
+                playerBox.appendChild(draggable)
+                createCard()
+            }
+
+        })
+
+        playerBox.addEventListener('drop', e => {
+            playerBox.setAttribute("empty", "false")
+        })
+    })
+    
+    function getDragAfterElement(box, y) {
+        var draggableElements = [...box.querySelectorAll('.maincontainer:not(.dragging)')]
+ 
+        return draggableElements.reduce((closest, child) => {
+            const boxBound = child.getBoundingClientRect()
+            const offset = y - boxBound.top - boxBound.height / 2
+            if (offset < 0 && offset > closest.offset) {
+                return { offset: offset, element: child } 
+            } else {
+                return closest
+            }
+        }, { offset: Number.NEGATIVE_INFINITY }).element
+    }
+}
+
+
+function battlePhase() {
+    
 }
--- script3.js
+++ script3.js
@@ -1,199 +1,45 @@
-var card,
-	image,
-	cardw,
-	cardh,
-	cardx,
-	cardy,
-	ocardx,
-	ocardy,
-	pinx,
-	piny,
-	pinxperc,
-	pinyperc,
-	targetx,
-	targety,
-	rx,
-	ry,
-	targetrx,
-	targetry,
-	scale,
-	targetscale,
-	ww,
-	wh,
-	md,
-	mx,
-	my,
-	whoosh,
-	whooshvol,
-	whooshvoltarget,
-	majesty,
-	majestyvol,
-	majestyvoltarget,
-	audioloaded;
+const list_items = document.querySelectorAll('.list-item');
+const lists = document.querySelectorAll('.list');
 
-createCard()
-function createCard() {
-	var cardDiv = document.createElement("DIV");
-	var cardImage = document.createElement("IMG");
+let draggedItem = null;
 
-	cardImage.setAttribute("src", "https://s3-us-west-2.amazonaws.com/s.cdpn.io/836/hearthstone-ragnaros.png");
-	cardImage.setAttribute("draggable", "true");
-	cardImage.classList.add("card-image");
+for (let i = 0; i < list_items.length; i++) {
+	const item = list_items[i];
 
-	cardDiv.classList.add("card");
-	cardDiv.appendChild(cardImage);
-	document.body.appendChild(cardDiv);
-}
+	item.addEventListener('dragstart', function () {
+		draggedItem = item;
+		setTimeout(function () {
+			item.style.display = 'none';
+		}, 0)
+	});
 
-function audioload() {
-	audioloaded++;
-	if( audioloaded == 2 ) {
-		document.body.classList.add( 'loaded' );
-		whoosh.play();
-		bindevents();
-		loop();
-	}
-}
+	item.addEventListener('dragend', function () {
+		setTimeout(function () {
+			draggedItem.style.display = 'block';
+			draggedItem = null;
+		}, 0);
+	})
 
-function init() {
-	onresize();
-	card = document.querySelector( '.card' );
-	image = document.querySelector( '.card-image' );
-	cardw = image.width;
-	cardh = image.height;
-	cardx = ww / 2 - cardw / 2;
-	cardy = wh / 2 - cardh / 2;
-	ocardx = cardx;
-	ocardy = cardy;
-	pinx = 0;
-	piny = 0;
-	pinxperc = 0;
-	pinyperc = 0;
-	targetx = cardx;
-	targety = cardy;
-	rx = 0;
-	ry = 0;
-	targetrx = 0;
-	targetry = 0;
-	scale = 1;
-	targetscale = scale;
-	md = false;
-	mx = cardx;
-	my = cardy;
-	audioloaded = 0;
-	
-	whooshvol = 0;
-	whooshvoltarget = 0;
-	whoosh = new Audio();
-	whoosh.addEventListener( 'canplaythrough', audioload );
-	whoosh.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/836/hs-whoosh.ogg';
-	whoosh.volume = 0;
-	whoosh.loop = true;
-	
-	majestyvol = 0;
-	majestyvoltarget = 0;
-	majesty = new Audio();
-	majesty.addEventListener( 'canplaythrough', audioload );
-	majesty.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/836/hs-majesty.ogg';
-	majesty.volume = 0;
-	majesty.loop = true;
-}
+	for (let j = 0; j < lists.length; j ++) {
+		const list = lists[j];
 
-function bindevents() {
-	card.addEventListener( 'mousedown', onmousedown );
-	window.addEventListener( 'mouseup', onmouseup );
-	window.addEventListener( 'mousemove', onmousemove );
-	window.addEventListener( 'resize', onresize );
-}
-
-function onmousedown( e ) {
-	md = true;
-	mx = e.pageX;
-	my = e.pageY;
-	pinx = cardw / 2; // pin to center
-	piny = cardh / 2; // pin to center
-	//pinx = mx - cardx; // pin to click point
-	//piny = my - cardy; // pin to click point
-	pinxperc = 100 - ( pinx / cardw ) * 100; // transform based on the pin position
-	pinyperc = 100 - ( piny / cardh ) * 100; // transform based on the pin position
-}
-
-function onmouseup() {
-	md = false;
-}
-
-function onmousemove( e ) {
-	if( md ) {
-		mx = e.pageX;
-		my = e.pageY;
-	}
-}
-
-function onresize() {
-	ww = window.innerWidth;
-	wh = window.innerHeight;
-}
-
-function loop() {
-	requestAnimationFrame( loop )
-	
-	// set new target position
-	targetx = mx - cardx - pinx;
-	targety = my - cardy - piny;
-	
-	// lerp to new position
-	cardx += targetx * 0.25;
-	cardy += targety * 0.25;
-	
-	// contain card to window bounds
-	if( cardx < -cardw / 2 ) {
-		cardx = -cardw / 2;
-	}
-	if( cardx > ww - cardw / 2 ) {
-		cardx = ww - cardw / 2;
-	}
-	if( cardy < -cardh / 2 ) {
-		cardy = -cardh / 2;
-	}
-	if( cardy > wh - cardh / 2 ) {
-		cardy = wh - cardh / 2;
-	}
-	
-	// get rotation based on how much card moved
-	targetrx = ( ocardy - cardy - rx ) * 3;
-	targetry = ( cardx - ocardx - ry ) * 3;
-	
-	// lock rotation so things don't get too crazy
-	targetrx = Math.min( targetrx, 90 );
-	targetrx = Math.max( targetrx, -90 );
-	targetry = Math.min( targetry, 90 );
-	targetry = Math.max( targetry, -90 );
-	
-	// lerp to new rotation
-	rx += targetrx * 0.1;
-	ry += targetry * 0.1;
+		list.addEventListener('dragover', function (e) {
+			e.preventDefault();
+		});
 		
-	// scale up when the mouse is pressed
-	targetscale = md ? 1.2 - scale : 1 - scale;
-	scale += targetscale * 0.2;
-	
-	// apply the transform
-	card.style[ 'transform' ] = 'translate3d(' + cardx + 'px, ' + cardy + 'px, 0)';
-	image.style[ 'transform-origin' ] = pinxperc + '% ' + pinyperc + '%';
-	image.style[ 'transform' ] = 'scale(' + scale + ') rotateY(' + ry + 'deg) rotateX(' + rx + 'deg)';
+		list.addEventListener('dragenter', function (e) {
+			e.preventDefault();
+			this.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+		});
 
-	majestyvoltarget = md ? 0.2 : 0;
-	majestyvol += ( majestyvoltarget - majestyvol ) * 0.1;
-	majesty.volume = majestyvol;
-	
-	whooshvoltarget = ( Math.abs( ( ocardy - cardy ) ) + Math.abs( ( ocardx - cardx ) ) ) * 0.003;
-	whooshvol += ( whooshvoltarget - whooshvol ) * 0.1;
-	whoosh.volume = Math.min( whooshvol, 1 );
-	
-	// store the old card position
-	ocardx = cardx;
-	ocardy = cardy;
+		list.addEventListener('dragleave', function (e) {
+			this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+		});
+
+		list.addEventListener('drop', function (e) {
+			console.log('drop');
+			this.append(draggedItem);
+			this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+		});
+	}
 }
-
-// pull up a chair by the hearth!
-window.onload = init;
