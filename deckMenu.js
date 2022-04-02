789357a17b79df30bf533a78901b2f8a4081636a
LyshaJaufar <lysha.ahmd@gmail.com>
2022-04-02 21:23:32 +0500

    upd

--- deckMenu.js
+++ deckMenu.js
@@ -2,7 +2,8 @@ import Deck from "./deck.js"
 
 var deck = new Deck()
 
-document.getElementsByClassName('sort-btn')[0].onclick = function() {
+
+document.getElementById("age").onclick = function() {
     console.log(deck.cards[0].frightFactor)
     console.log("")
 };
